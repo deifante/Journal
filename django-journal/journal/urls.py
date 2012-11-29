b2d132c43e25c0314c904b78f049ca3ad1f7f311
Deifante Walters <deifante@gmail.com>
2012-11-29 15:49:31 -0700

    ...

--- django-journal/journal/urls.py
+++ django-journal/journal/urls.py
@@ -4,13 +4,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'journal.views.home', name='home'),
-    # url(r'^journal/', include('journal.foo.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^todos/$', 'todos.views.index'),
+    url(r'^todos/(?P<todo_id>\d+)/$', 'todos.views.detail'),
     # Uncomment the admin/doc line below to enable admin documentation:
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- django-journal/todos/views.py
+++ django-journal/todos/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.template import Context, loader
+from todos.models import TodoList
+from django.http import HttpResponse
+
+def index(request):
+    
